f84f014b918c7d4ca4a8ab4fb25251e2d79c8897
alex <alex@openpy.org>
2021-09-26 13:40:21 +0800

    remove unuseful interface

--- sha3/crypto.go
+++ sha3/crypto.go
@@ -1,7 +1,6 @@
 package sha3
 
 import (
-	"hash"
 	"math/big"
 )
 
@@ -10,27 +9,6 @@ var (
 	secp256k1N, _  = new(big.Int).SetString("fffffffffffffffffffffffffffffffebaaedce6af48a03bbfd25e8cd0364141", 16)
 	secp256k1halfN = new(big.Int).Div(secp256k1N, big.NewInt(2))
 )
-
-type KeccakState interface {
-	hash.Hash
-	Read([]byte) (int, error)
-}
-
-// NewKeccakState creates a new KeccakState
-func NewKeccakState() KeccakState {
-	return NewLegacyKeccak256().(KeccakState)
-}
-
-// Keccak256 calculates and returns the Keccak256 hash of the input data.
-func Keccak256(data ...[]byte) []byte {
-	b := make([]byte, 32)
-	d := NewKeccakState()
-	for _, b := range data {
-		d.Write(b)
-	}
-	d.Read(b)
-	return b
-}
 
 // ValidateSignatureValues verifies whether the signature values are valid with
 // the given chain rules. The v value is assumed to be either 0 or 1.
